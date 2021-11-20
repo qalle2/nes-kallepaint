f3c12a0866d663b4d98a3b6a2f3f2cb8922f2aa4
qalle2 <qalle85@gmail.com>
2021-11-20 06:57:02 +0200

    update script

--- chr-background-generate.py
+++ chr-background-generate.py
@@ -1,44 +1,34 @@
-"""Generate background CHR ROM data for Kalle Paint.
-- 256 tiles
-- 4 KiB
-- all combinations of 2*2 subpixels * 4 colors
-- bits of tile index: AaBbCcDd
-    - Aa = top left subpixel
-    - Bb = top right subpixel
-    - Cc = bottom left subpixel
-    - Dd = bottom right subpixel
-    - capital letter = MSB, small letter = LSB"""
+# generate background CHR ROM data for Kalle Paint
+# - 256 tiles
+# - 4 KiB
+# - all combinations of 2*2 subpixels and 4 colors
+# - bits of tile index: AaBbCcDd
+#     Aa = top    left  subpixel
+#     Bb = top    right subpixel
+#     Cc = bottom left  subpixel
+#     Dd = bottom right subpixel
+#     (capital letter = MSB, small letter = LSB)
 
-import os
-import sys
+import os, sys
 
-def chr_byte(tileIndex, leftPos, rightPos):
-    """leftPos/rightPos: bit position to extract left/right half from
-    return: NES CHR byte (8*1 pixels, 1 bitplane)"""
-
-    return (((tileIndex >> leftPos) & 1) * 0xf0) | (((tileIndex >> rightPos) & 1) * 0x0f)
+def chr_byte(i, l, r):
+    # i: tile index, l/r: bit position to extract left/right half from
+    # return: NES CHR byte (8*1 pixels, 1 bitplane)
+    return (((i >> l) & 1) * 0xf0) | (((i >> r) & 1) * 0x0f)
 
 def main():
     if len(sys.argv) != 2:
-        sys.exit("Argument: output_file")
-    fileName = sys.argv[1]
-    if os.path.exists(fileName):
-        sys.exit("File already exists.")
+        sys.exit("Argument: output file (will be overwritten)")
 
     data = bytearray()
-    for tileIndex in range(0x100):
-        # top/bottom half of less significant bitplane
-        data += 4 * bytes((chr_byte(tileIndex, 6, 4),))
-        data += 4 * bytes((chr_byte(tileIndex, 2, 0),))
-        # top/bottom half of more significant bitplane
-        data += 4 * bytes((chr_byte(tileIndex, 7, 5),))
-        data += 4 * bytes((chr_byte(tileIndex, 3, 1),))
+    for i in range(0x100):  # tile index
+        data += 4 * bytes((chr_byte(i, 6, 4),))  # less significant bitplane, top    half
+        data += 4 * bytes((chr_byte(i, 2, 0),))  # less significant bitplane, bottom half
+        data += 4 * bytes((chr_byte(i, 7, 5),))  # more significant bitplane, top    half
+        data += 4 * bytes((chr_byte(i, 3, 1),))  # more significant bitplane, bottom half
 
-    with open(fileName, "wb") as target:
+    with open(sys.argv[1], "wb") as target:
         target.seek(0)
         target.write(data)
 
-    print(f"{fileName} written.")
-
-if __name__ == "__main__":
-    main()
+main()
