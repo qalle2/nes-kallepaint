511d995e23a6d14ebaca86e0fdcbff9808eecade
qalle2 <qalle85@gmail.com>
2022-04-26 01:29:04 +0300

    add hex source

--- assemble.sh
+++ assemble.sh
@@ -1,6 +1,6 @@
 # Note: this script will DELETE files. Use at your own risk.
 
-rm -f *.bin *.gz *.nes
+rm -f *.bin *.cdl *.gz *.nes *.nl
 python3 chr-bg-gen.py chr-bg.bin
 python3 ../nes-util/nes_chr_encode.py chr-spr.png chr-spr.bin
 asm6 paint.asm paint.nes
