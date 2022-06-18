ab3706258d8f2a3e9d72a758b337e9c50fc351db
qalle2 <qalle85@gmail.com>
2022-06-18 03:57:09 +0300

    CHR data generator

--- chr-bg-gen.py
+++ chr-bg-gen.py
@@ -12,25 +12,22 @@ Generate background CHR ROM data for Kalle Paint.
   example: tile %10001011 = $00000000 $0f0f0f0f $f0f0f0f0 $ffffffff
 """
 
-# TODO: do it in the manner described above
-
 import os, sys
 
-def chr_byte(i, l, r):
-    # i: tile index, l/r: bit position to extract left/right half from
-    # return: NES CHR byte (8*1 pixels, 1 bitplane)
-    return (((i >> l) & 1) * 0xf0) | (((i >> r) & 1) * 0x0f)
+def generate_bytes():
+    for i in range(0x100):  # tile index
+        yield (i >> 6 & 1) * 0xf0 | (i >> 4 & 1) * 0x0f
+        yield (i >> 2 & 1) * 0xf0 | (i      & 1) * 0x0f
+        yield (i >> 7 & 1) * 0xf0 | (i >> 5 & 1) * 0x0f
+        yield (i >> 3 & 1) * 0xf0 | (i >> 1 & 1) * 0x0f
 
 def main():
     if len(sys.argv) != 2:
         sys.exit("Argument: output file (will be overwritten)")
 
     data = bytearray()
-    for i in range(0x100):  # tile index
-        data += 4 * bytes((chr_byte(i, 6, 4),))  # less significant bitplane, top    half
-        data += 4 * bytes((chr_byte(i, 2, 0),))  # less significant bitplane, bottom half
-        data += 4 * bytes((chr_byte(i, 7, 5),))  # more significant bitplane, top    half
-        data += 4 * bytes((chr_byte(i, 3, 1),))  # more significant bitplane, bottom half
+    for byte in generate_bytes():
+        data += 4 * bytes((byte,))
 
     with open(sys.argv[1], "wb") as target:
         target.seek(0)
